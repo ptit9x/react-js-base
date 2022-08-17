88efebe06a1c9b2bae5a68d8dd7d050c1d989ad8
Do Long <doduclong2208@gmail.com>
2022-08-17 22:34:03 +0700

    clear example

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -17,7 +17,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
-        <p>chao abc</p>
         <Routes />
       </Suspense>
     </BrowserRouter>
--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,17 +1,10 @@
-import React, { ReactNode } from "react";
-import { useTranslation } from "react-i18next";
+import { ReactNode } from "react";
+
 interface Props {
   children: ReactNode;
 }
 
 const AuthLayout = ({ children }: Props) => {
-  const { t } = useTranslation();
-
-  return (
-    <div>
-      <p>{t("auth-layout")}</p>
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 };
 export default AuthLayout;
