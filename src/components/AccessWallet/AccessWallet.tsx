9fa9cf63563f88c4e4f1999f1ad2a548c42c9f61
Long_DD <o_long_duc.d@kbtg.tech>
2022-08-17 15:26:38 +0700

    create access wallet item

--- src/components/AccessWallet/AccessWallet.tsx
+++ src/components/AccessWallet/AccessWallet.tsx
@@ -1,14 +1,39 @@
+import { Grid } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import AccessWalletItem from "../AccessWalletItem/AccessWalletItem";
 
 const AccessWallet = () => {
   const { t } = useTranslation();
-
+  const buttons = [
+    {
+      srcIcon:
+        "https://www.myetherwallet.com/img/icon-enkrypt-block.cb05ee30.svg",
+      title: "Enkrypt",
+      description: "Connect with Enkrypt browser extension",
+      isOfficial: true
+    },
+    {
+      srcIcon: "https://www.myetherwallet.com/img/icon-extensions.fbf7b80e.png",
+      title: "Browser extension",
+      description: "Use your Web3 wallet with MEW",
+      isOfficial: true
+    }
+  ];
   return (
-    <div>
-      <AccessWalletItem />
-      <AccessWalletItem />
-    </div>
+    <Grid container spacing={2} direction="column">
+      {buttons.map(btnContext => {
+        return (
+          <Grid item xs={8}>
+            <AccessWalletItem
+              srcIcon={btnContext.srcIcon}
+              title={btnContext.title}
+              description={btnContext.description}
+              isOfficial={btnContext.isOfficial}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
   );
 };
 export default AccessWallet;
--- src/components/AccessWalletItem/AccessWalletItem.tsx
+++ src/components/AccessWalletItem/AccessWalletItem.tsx
@@ -1,9 +1,85 @@
-import { Button } from "@mui/material";
+import { Button, Chip, Grid, Typography } from "@mui/material";
+import GppGoodIcon from "@mui/icons-material/GppGood";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 
-const AccessWalletItem = () => {
+const BREAK_POINT_LAYOUT = 960;
+const Img = styled.img`
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%"
+`;
+interface Props {
+  srcIcon: string;
+  title: string;
+  description: string;
+  isOfficial: boolean;
+}
+
+const AccessWalletItem = (props: Props) => {
   const { t } = useTranslation();
+  const [changeLayout, setChangeLayout] = useState(false);
+  function handleChangeInnerSize() {
+    if (window.innerWidth < BREAK_POINT_LAYOUT) {
+      setChangeLayout(true);
+    } else {
+      setChangeLayout(false);
+    }
+  }
+  useEffect(() => {
+    handleChangeInnerSize();
 
-  return <Button variant="text">Text</Button>;
+    window.addEventListener("resize", handleChangeInnerSize);
+    return () => window.removeEventListener("resize", handleChangeInnerSize);
+  }, []);
+
+  return (
+    <Button variant="contained" color="success">
+      <Grid container spacing={4}>
+        {!changeLayout ? (
+          <Grid item>
+            <Img width="70" height="70" alt={props.title} src={props.srcIcon} />
+          </Grid>
+        ) : null}
+        <Grid item container sm>
+          <Grid item container alignItems="center" spacing={2}>
+            {changeLayout ? (
+              <Grid item>
+                <Img
+                  width="50"
+                  height="50"
+                  alt={props.title}
+                  src={props.srcIcon}
+                />
+              </Grid>
+            ) : null}
+            <Grid item>
+              <Typography gutterBottom variant="subtitle1">
+                {props.title}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs>
+            <Typography align="left" variant="body2" gutterBottom>
+              {props.description}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      {props.isOfficial ? (
+        <Grid item>
+          <Chip
+            size="small"
+            icon={<GppGoodIcon />}
+            label="Official"
+            color="success"
+          />
+        </Grid>
+      ) : null}
+    </Button>
+  );
 };
 export default AccessWalletItem;
