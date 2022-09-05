e8d1b967d5db5c9fe0335bc6392f8c3198fb9b80
trungphuong2k <trungthiphuong2k@gmail.com>
2022-09-05 14:58:27 +0700

    navbar

--- src/layouts/MainLayout/Navbar/Navbar.tsx
+++ src/layouts/MainLayout/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { useTranslation } from "react-i18next";
+import { ButtonCusTom } from "src/assets/common.styled";
 import icon_open_sidebar from "src/assets/icons/icon-open-sidebar.svg";
 import { BREAKPOINT } from "src/constants/styles";
 import { useAppDispatch } from "src/store";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import theme from "src/theme";
 
 import { onOpenSidebar } from "../MainLayout.reducer";
@@ -9,22 +13,49 @@ import { OpenSidebarButton } from "./Navbar.styled";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
   return (
-    <Box
-    // style={{
-    //   width: "100%",
-    //   backgroundColor: theme.palette.background.paper,
-    //   minHeight: "60px",
-    // }}
+    <Grid2
+      container
+      sx={{ my: 2 }}
+      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      align-center
     >
-      <OpenSidebarButton
-        onClick={() => dispatch(onOpenSidebar())}
-        BREAKPOINT={BREAKPOINT.XL}
+      <Grid2 xs={12} md={8}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box>
+            <Typography fontWeight={theme.typography.fontWeightBold}>
+              {t("you-can-now-buy-crypto")}
+            </Typography>
+            <Typography
+              fontWeight={theme.typography.fontWeightLight}
+              fontSize={theme.typography.fontSize}
+              color={theme.palette.text.secondary}
+            >
+              {t("enjoy-low-fee")}
+            </Typography>
+          </Box>
+          <ButtonCusTom>{t("buy-crypto-now")}</ButtonCusTom>
+        </Box>
+        <OpenSidebarButton
+          onClick={() => dispatch(onOpenSidebar())}
+          BREAKPOINT={BREAKPOINT.XL}
+        >
+          <img src={icon_open_sidebar} alt="open-sidebar" width="40px" />
+        </OpenSidebarButton>
+      </Grid2>
+      <Grid2
+        xs={12}
+        md={4}
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignSelf: "center",
+        }}
       >
-        <img src={icon_open_sidebar} alt="open-sidebar" width="40px" />
-      </OpenSidebarButton>
-      Navbar
-    </Box>
+        <NotificationsNoneIcon></NotificationsNoneIcon>
+      </Grid2>
+    </Grid2>
   );
 };
 
