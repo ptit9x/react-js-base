05d5efdcfc5741569cc9200bb79c2067e56d204a
Long_DD <o_long_duc.d@kbtg.tech>
2022-09-07 21:16:35 +0700

    add suspense for guest layout

--- src/layouts/GuestLayout/GuestLayout.tsx
+++ src/layouts/GuestLayout/GuestLayout.tsx
@@ -1,7 +1,9 @@
 import { Box, Grid } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 import styled from "@emotion/styled";
 import Header from "./Header";
+
 const BoxStyled = styled(Box)`
   background-color: ${({ theme }) => theme.palette.primary.main};
   padding-bottom: 70px;
@@ -19,7 +21,9 @@ const GuestLayout = () => {
           justifyContent="center"
           alignItems="center"
         >
-          <Outlet />
+          <Suspense fallback={<div />}>
+            <Outlet />
+          </Suspense>
         </Grid>
       </BoxStyled>
     </div>
